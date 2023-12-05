61fe6736a80d09333b649b1fee4cc4e0a7b70746
deviprasadkl <deviprasadofficial@gmail.com>
2023-12-05 16:53:51 +0530

    Added custom hook to fetch name and email id

--- client-dashboard/src/views/dashboard/customHooks/useFetchUserDetails.js
+++ client-dashboard/src/views/dashboard/customHooks/useFetchUserDetails.js
@@ -1,14 +1,12 @@
 export default function useFetchUserDetails(details) {
+    let userData = localStorage.getItem('user');
+    userData = JSON.parse(userData);
     if(details == "email"){
-        let userData = localStorage.getItem('user');
-        userData = JSON.parse(userData);
         if( userData ){
             return userData.email;
         }
         return "None";
     } else if (details == "name"){
-        let userData = localStorage.getItem('user');
-        userData = JSON.parse(userData);
         if( userData ){
             return userData.name;
         }
