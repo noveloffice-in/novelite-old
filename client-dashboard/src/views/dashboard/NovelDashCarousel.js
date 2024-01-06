3012a1d23b23247a9ea953f1b41e58d3410f057c
deviprasadkl <deviprasadofficial@gmail.com>
2024-01-06 09:38:46 +0000

    Novel Images added to dashboard

--- client-dashboard/src/views/dashboard/NovelDashCarousel.js
+++ client-dashboard/src/views/dashboard/NovelDashCarousel.js
@@ -10,27 +10,34 @@ import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 
+//Images
+import img1 from '../../assets/images/offices/1.webp'
+import img2 from '../../assets/images/offices/2.webp'
+import img3 from '../../assets/images/offices/3.webp'
+import img4 from '../../assets/images/offices/4.webp'
+import img5 from '../../assets/images/offices/5.webp'
+
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 const images = [
     {
         label: 'San Francisco – Oakland Bay Bridge, United States',
-        imgPath:
-            'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+        imgPath: `${img1}`,
     },
     {
         label: 'Bird',
-        imgPath:
-            'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+        imgPath:`${img2}`,
     },
     {
         label: 'Bali, Indonesia',
-        imgPath:
-            'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+        imgPath: `${img3}`,
     },
     {
         label: 'Goč, Serbia',
-        imgPath:
-            'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+        imgPath:`${img4}`,
+    },
+    {
+        label: 'Goč, Serbia',
+        imgPath:`${img5}`,
     },
 ];
 
