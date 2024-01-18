e2191408a73f535538b2ad2073beeae38c46dbdd
deviprasadkl <deviprasadofficial@gmail.com>
2024-01-18 11:56:58 +0000

    First different

---
+++ novelite/novel_bookings/doctype/room_bookings/test_room_bookings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Ragul KM and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRoomBookings(FrappeTestCase):
+	pass
